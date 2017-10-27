7cb5dbf33bc704f347bd250b051add51244969de
scottyplunkett <splunkett@8thLight.com>
2017-10-26 19:57:01 -0500

    board_full function passing

--- tictactoe/src/utilities.erl
+++ tictactoe/src/utilities.erl
@@ -3,13 +3,14 @@
          mark_board/3,
          change_player/1,
          validate_move_position/2,
-         restart_move/2]).
+         restart_turn/2,
+         board_full/1]).
 
 move(Location, Board, Piece) ->
   Position = validate_move_position(console_io:validate_move_format(Location), Board),
   case Position of
     invalid ->
-      restart_move(Board,Piece);
+      restart_turn(Board,Piece);
     _ ->
       mark_board(Position, Board, Piece)
   end.
@@ -32,6 +33,15 @@ validate_move_position(Position, Board) ->
       invalid
   end.
   
-restart_move(Board,Piece) ->
+restart_turn(Board,Piece) ->
   console_io:display("Invalid Move, try again...\n"),
-    game_loop:take_turn(Board, Piece).
+  game_loop:take_turn(Board, Piece).
+
+board_full([ThisCell | _]) when is_integer(ThisCell) ->
+  false;
+board_full([_ | RestOfCells]) ->
+  board_full(RestOfCells);
+board_full([]) ->
+  true.
+
+
--- tictactoe/test/utilities_tests.erl
+++ tictactoe/test/utilities_tests.erl
@@ -17,11 +17,26 @@ change_player_test() ->
 
 validate_move_test() ->
   [{  "validates a move",
-      ?assertEqual(
-        utilities:validate_move_position(1,[o,2,3,4,5,6,7,8,9]),invalid),
-      ?assertEqual( 
-        utilities:validate_move_position(10,[1,2,3,4,5,6,7,8,9]),invalid),
-      ?assertEqual( 
-        utilities:validate_move_position(1,[1,2,3,4,5,6,7,8,9]),1),
-      ?assertEqual( 
-        utilities:validate_move_position(0,[1,2,3,4,5,6,7,8,9]),invalid)}].
+      ?assertEqual( invalid,
+        utilities:validate_move_position(1,[o,2,3,4,5,6,7,8,9])),
+      ?assertEqual( invalid,
+        utilities:validate_move_position(10,[1,2,3,4,5,6,7,8,9])),
+      ?assertEqual( 1,
+        utilities:validate_move_position(1,[1,2,3,4,5,6,7,8,9])),
+      ?assertEqual( invalid,
+        utilities:validate_move_position(0,[1,2,3,4,5,6,7,8,9]))}].
+
+board_full_test() -> 
+  [{  "checks if the game is over",
+      ?assertEqual( false,
+        utilities:board_full([1,2,3,4,5,6,7,8,9])),
+      ?assertEqual( false,
+        utilities:board_full([x,2,3,4,5,6,7,8,9])),
+      ?assertEqual( false,
+        utilities:board_full([x,o,3,4,5,6,7,8,9])),
+      ?assertEqual( false,
+        utilities:board_full([x,o,x,o,5,x,o,x,o])),
+      ?assertEqual( true,
+        utilities:board_full([x,o,x,o,x,x,o,x,o]))}].
+
+
