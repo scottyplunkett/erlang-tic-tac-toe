34e28259a1903d51d279e5fd98fdde9d955edb7a
Scott Plunkett <splunkett@8thlight.com>
2017-11-02 11:03:28 -0500

    Update instructions_tests.erl

--- tictactoe/test/instructions_tests.erl
+++ tictactoe/test/instructions_tests.erl
@@ -1,10 +1,8 @@
 -module(instructions_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-greet_test() -> 
-	[{ "passes a greeting message to a client",
-		 ?assertEqual("Welcome to TicTacToe\n", instructions:greet())}].
+greet_test() 	    -> [{ "passes a greeting message to a client",
+			   ?assertEqual("Welcome to TicTacToe\n", instructions:greet())}].
 
-ask_for_move_test() ->
-  [{ "passes a message asking for a move",
-     ?assertEqual("Where would you like to place your marker?",instructions:ask_for_move())}].
+ask_for_move_test() -> [{ "passes a message asking for a move",
+			   ?assertEqual("Where would you like to place your marker?",instructions:ask_for_move())}].
