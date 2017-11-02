41e33f67b201441233bbc218ce8f916229243f91
Scott Plunkett <splunkett@8thlight.com>
2017-11-02 10:17:24 -0500

    Update console_io.erl

--- tictactoe/src/console_io.erl
+++ tictactoe/src/console_io.erl
@@ -1,34 +1,22 @@
 -module(console_io).
--export([	display/1,
-          display_board/1,
-          greet_user/0,
-          get_move/0,
-          validate_move_format/1,
-          prompt/1]).
+-export([display/1,display_board/1,greet_user/0,get_move/0,validate_move_format/1,prompt/1]).
 
+display(Message) -> io:fwrite(Message). 
 
-display(Message) ->
-  io:fwrite(Message). 
-
-display_board([]) -> board;
-display_board([RowToPrint | RemainingRows]) ->
-  io:format("~w~n",[RowToPrint]),
-  display_board(RemainingRows).
+display_board([]) 			    
+	-> board;
+display_board([RowToPrint | RemainingRows]) 
+	-> io:format("~w~n",[RowToPrint]),
+  	   display_board(RemainingRows).
   
 greet_user() -> display(instructions:greet()).
 
-prompt(MessageToClient) -> 
-	io:fread(MessageToClient++"\n","~s").
+prompt(MessageToClient) -> io:fread(MessageToClient++"\n","~s").
 
 get_move() -> prompt(instructions:ask_for_move()).
 
-validate_move_format(UserMove) ->
-  {ok,[Move]} = UserMove,
-  try list_to_integer(Move) of 
-    _ ->
-      list_to_integer(Move)
-  catch 
-    error:badarg ->
-      invalid
-  end.
-
+validate_move_format(UserMove) 
+	-> {ok,[Move]} = UserMove,
+		try list_to_integer(Move) of _ -> list_to_integer(Move)
+  		catch error:badarg 	       -> invalid
+		end.
