6c261130e6f6e858127fb83e40fdb03e8b119379
Scott Plunkett <splunkett@8thlight.com>
2017-11-02 10:25:09 -0500

    Update game_loop.erl

--- tictactoe/src/game_loop.erl
+++ tictactoe/src/game_loop.erl
@@ -1,14 +1,12 @@
 -module(game_loop).
 -export([begin_play/0,take_turn/1,take_turn/2, complete_turn/1, complete_turn/2, setup/0,restart_turn/2]).
 
+begin_play()                     -> Board = format_board:make_new_board(), 
+                                    setup(),
+                                    console_io:display_board(format_board:split_board_into_rows(Board)),
+                                    take_turn(Board, x).
 
-begin_play()  
-  -> Board = format_board:make_new_board(), 
-     setup(),
-     console_io:display_board(format_board:split_board_into_rows(Board)),
-     take_turn(Board, x).
-
-setup() -> console_io:greet_user().
+setup()                          -> console_io:greet_user().
 
 take_turn(Board, Piece)          -> take_turn(utilities:move(console_io:get_move(), Board, Piece)).
 take_turn({invalid,Board,Piece}) -> game_loop:restart_turn(Board,Piece);
@@ -21,5 +19,5 @@ complete_turn(Board, false)      -> complete_turn(Board, utilities:board_full(Bo
 complete_turn(_, cats_game)      -> console_io:display("Draw!\n");
 complete_turn(Board, not_yet)    -> take_turn(Board, utilities:whos_in_play(Board)).
 
-restart_turn(Board,Piece) -> console_io:display("Invalid Move, try again...\n"), 
-                             take_turn(Board, Piece).
+restart_turn(Board,Piece)        -> console_io:display("Invalid Move, try again...\n"), 
+                                    take_turn(Board, Piece).
