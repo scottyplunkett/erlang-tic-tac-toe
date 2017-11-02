8155a6f03957ea1850e3a3474d141035cef6c75f
Scott Plunkett <splunkett@8thlight.com>
2017-11-02 11:01:52 -0500

    Update game_loop_tests.erl

--- tictactoe/test/game_loop_tests.erl
+++ tictactoe/test/game_loop_tests.erl
@@ -10,24 +10,3 @@ complete_turn_test() ->
                    game_loop:complete_turn([x,2,3,4,x,6,7,8,x])),
       ?assertEqual(console_io:display("Draw!\n"),
                    game_loop:complete_turn([x,o,x,o,x,x,o,x,o]))}].
-
-% ?assertEqual(game_loop:take_turn([x,o,x,4,x,x,7,x,9], x),
-%              utilities:complete_turn([x,o,x,4,x,x,7,x,9], o))}].
-
-
-% begin_play_test() -> 
-%   [{ "begins play, displays initial board",
-%       ?assert(console_io:greet_user() 
-%               =:= 
-%               game_loop:begin_play("greet")),
-%       ?assert(console_io:display_board(
-%                 format_board:split_board_into_rows(
-%                   format_board:make_new_board())) 
-%               =:= 
-%               game_loop:begin_play("make and display new board")),
-%       ?assert({player1,player2}
-%               =:=
-%               game_loop:begin_play("get player markers"))}].
-% % take_turn_test() -> 
-% %   [{ "allows the player in turn, to make a move, than switches the players and calls itself",
-% %       ?assertEqual()}]
