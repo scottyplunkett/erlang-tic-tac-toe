de8ebc8862f64c98e2f6670158411eb9ac6ff7a6
scottyplunkett <splunkett@8thLight.com>
2017-11-07 13:01:39 -0600

    added setup mode

--- tictactoe/src/game_loop.erl
+++ tictactoe/src/game_loop.erl
@@ -1,15 +1,29 @@
 -module(game_loop).
--export([begin_play/0,take_turn/1,take_turn/2, complete_turn/1, complete_turn/2, setup/0,restart_turn/2]).
+-export([begin_play/0,take_turn/1,take_turn/2, complete_turn/1, complete_turn/2, setup/0,restart_turn/2,get_mode_move/1,get_mode_move/3]).
 -define(INIT_BOARD, [1,2,3,4,5,6,7,8,9]).
 
 begin_play()                     -> Board = ?INIT_BOARD,
-                                    setup(),
+                                    Mode = setup(),
                                     console_io:display_board(Board),
-                                    take_turn(Board, x).
+                                    take_turn(Board,maps:get(player1, Mode)).
 
-setup()                          -> console_io:greet_user().
+setup()                          -> console_io:greet_user(),
+                                    console_io:display("What game mode would you like to play?\n"),
+                                    console_io:display("1: Human vs Human\n"),
+                                    console_io:display("2: Human vs Computer\n"),
+                                    console_io:display("3: Computer vs Computer\n"),
+                                    {ok,Option} = console_io:prompt("Option>"),
+                                    setup:set_players(list_to_integer(Option)).
 
-take_turn(Board, Piece)          -> take_turn(utilities:move(console_io:get_move(), Board, Piece)).
+get_mode_move(Board,Piece,human)    ->  utilities:move(console_io:get_move(), Board, Piece);
+get_mode_move(Board,Piece,computer) ->  ai:get_best_move(Board,Piece).
+
+get_mode_move(Board)                ->  case utilities:whos_in_play(Board) of 
+                                              x ->  get_mode_move(Board,x,maps:get(player1));
+                                              o ->  get_mode_move(Board,o,maps:get(player2))
+                                        end.
+
+take_turn(Board, Piece)          -> take_turn(utilities:move(get_mode_move(Board), Board, Piece)).
 take_turn({invalid,Board,Piece}) -> game_loop:restart_turn(Board,Piece);
 take_turn(NewBoard)              -> console_io:display_board(NewBoard),
                                     complete_turn(NewBoard).
